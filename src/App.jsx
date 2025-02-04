ddef44c982fcd94ff0d6f97e9294f3779a85c62a
Susep Sp <susepsupriatna@gmail.com>
2025-02-04 08:12:20 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import LineChart from './pages/LineChart';
 import BarChart from './pages/BarChart';
